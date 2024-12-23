ad9940b04e0785477f9e2a6dabcb6a3f2f7e617e
jade floase <jadeharley0409@proton.me>
2024-12-22 19:38:15 -0600

    added more photos to photos, and updated photo's code

--- randomerjunk/photos.js
+++ randomerjunk/photos.js
@@ -1,40 +1,17 @@
 function hide(){
-   document.getElementById('bigimg').innerHTML = ''; 
-   document.getElementById('ldcds').setAttribute("width", "300px");
-   document.getElementById('myst').setAttribute("width", "300px");
-   document.getElementById('cars').setAttribute("width", "300px");
-   document.getElementById('dino').setAttribute("width", "300px");
+	document.getElementById('bigimg').innerHTML = ''; 
+	document.getElementById('indiv').className = 'medimg';
    
 }
 
-function ldcds(){
- document.getElementById('bigimg').innerHTML = '<img src=https://i.imgur.com/7G9j0eT.jpg onclick=hide()>';  
-   document.getElementById('ldcds').setAttribute("width", "100px"); 
-   document.getElementById('myst').setAttribute("width", "100px");
-   document.getElementById('cars').setAttribute("width", "100px");
-   document.getElementById('dino').setAttribute("width", "100px");
-}
-
-function myst(){
- document.getElementById('bigimg').innerHTML = '<img src=https://i.imgur.com/EbTuMLN.jpg   onclick=hide()>';  
-   document.getElementById('ldcds').setAttribute("width", "100px"); 
-   document.getElementById('myst').setAttribute("width", "100px");
-   document.getElementById('cars').setAttribute("width", "100px");
-   document.getElementById('dino').setAttribute("width", "100px");
-}
-
-function cars(){
- document.getElementById('bigimg').innerHTML = '<img src=https://i.imgur.com/AVLAjw0.jpg   onclick=hide()>';  
-   document.getElementById('ldcds').setAttribute("width", "100px"); 
-   document.getElementById('myst').setAttribute("width", "100px");
-   document.getElementById('cars').setAttribute("width", "100px");
-   document.getElementById('dino').setAttribute("width", "100px");
-}
-
-function dino(){
- document.getElementById('bigimg').innerHTML = '<img src=https://i.imgur.com/tKqV7I4.jpg   onclick=hide()>';  
-   document.getElementById('ldcds').setAttribute("width", "100px"); 
-   document.getElementById('myst').setAttribute("width", "100px");
-   document.getElementById('cars').setAttribute("width", "100px");
-   document.getElementById('dino').setAttribute("width", "100px");
+function newswitch(url, title){
+	document.getElementById('indiv').className = 'smallimg';
+	switch(title){
+		case(undefined):
+			document.getElementById('bigimg').innerHTML = '<img src=https://i.imgur.com/'+url+'.jpg   onclick=hide()>';  
+			break;
+		default: 
+			document.getElementById('bigimg').innerHTML = '<h1>'+title+'</h1><img src=https://i.imgur.com/'+url+'.jpg   onclick=hide()>';  
+			break;
+	}
 }
