e5093693e1d3c847da323d1b73200252d116eabc
jade floase <jadeharley0409@proton.me>
2024-12-22 18:03:06 -0600

    fixed some spelling on the art page, and added some new albums!

--- randomerjunk/cds.js
+++ randomerjunk/cds.js
@@ -9,7 +9,25 @@ function showrev(){
 function spike(){
  document.getElementById('title').innerHTML= '<span>Elvis Costello - Spike</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/kimh6ae.jpg");  
- document.getElementById('para').innerHTML = '<span>this isn&apos;t my favorite album, but it still holds a special place in my heart. my dad gave me this cd when i first got a walkman, and thus i&apos;ve listened to it for... a while. again, it&apos;s not my favorite album anymore, but it&apos;s still good! <br> the instrumentals are pretty alright, and the lyrics are great on their own, but i feel like elvis&apos;s incredible preformance distracts from them. <br> spike is still great though! i like it a lot :) <br><br> 7/10 - favorite track: ms macbeth </span>'; 
+ document.getElementById('para').innerHTML = '<span>this isn&apos;t my favorite album, but it still holds a special place in my heart. my dad gave me this cd when i first got a walkman, and thus i&apos;ve listened to it for... a while. again, it&apos;s not my favorite album anymore, but it&apos;s still good! <br> the instrumentals are pretty alright, and the lyrics are great on their own, but i feel like elvis&apos;s incredible performance distracts from them. <br> spike is still great though! i like it a lot :) <br><br> 7/10 - favorite track: ms macbeth </span>'; 
+}
+
+function dookie(){
+ document.getElementById('title').innerHTML= '<span>Green Day - Dookie</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/E5fGWPG.jpg");  
+ document.getElementById('para').innerHTML = '<span>a pretty good album, focusing on a shit(ish) guy with a shit life<br><br> 7/10 - favorite track: welcome to paradise</span>'; 
+}
+
+function americanidiot(){
+ document.getElementById('title').innerHTML= '<span>Green Day - American Idiot</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/LOCqguC.jpg");  
+ document.getElementById('para').innerHTML = '<span>an incredible album, focusing on a young, kinda idiotic guy with a shit upbringing. its got a great story, and captures a lot of emotions incredibly well! please go listen to it<br><br> 9/10 - favorite track: give me novacaine/track 12 (homecoming)</span>'; 
+}
+
+function owt(){
+ document.getElementById('title').innerHTML= '<span>Lemon Demon - One Weird Trip (single)</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/R11zgh9.jpg");  
+ document.getElementById('para').innerHTML = '<span>this cd has two songs - both of them being the most modern lemon demon songs currently on cd. they&apos;re both really good! the cd&apos;s art is themed around old aol free trial disks, and there&apos;s a web rendition of a windows xp desktop included on the disk :D<br><br> 7/10 - favorite track: um uh um uh um one weird trip </span>'; 
 }
 
 function spiritphone(){
@@ -21,25 +39,31 @@ function spiritphone(){
 function naturetapes(){
  document.getElementById('title').innerHTML= '<span>Lemon Demon - Nature Tapes</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/MFDUaWw.jpg");  
- document.getElementById('para').innerHTML = '<span>insane, halarious, and the only time neil spouts profanity, this EP is the greatest thing to come from him. ever. save it for when you feel like shit <br> it uses a cardboard case. i don&apos;t really like that :(<br><br> 10/10 - favorite track: my trains</span>'; 
+ document.getElementById('para').innerHTML = '<span>insane, hilarious, and the only time neil spouts profanity, this EP is the greatest thing to come from him. ever. save it for when you feel like shit <br> it uses a cardboard case. i don&apos;t really like that :(<br><br> 10/10 - favorite track: my trains</span>'; 
 }
 
 function iambecome(){
  document.getElementById('title').innerHTML= '<span>Lemon Demon - I Am Become Christmas</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/NOqU8Nu.jpg");  
- document.getElementById('para').innerHTML = '<span> -- bought on vinyl -- <br><br>this is neil&apos;s first ep, and the harbinger of the "modern" lemon demon era <br>its on the shorter side of lemon demon projects (~15 minutes long), but each and every song is unique and interesting :D <br> from being depressed due to snow, to actual body horror, theres a lot packed into this release <br> i also have the cd release now!! it uses a cardboard case. buyer beware! :( <br><br> 8/10 - favorite track: aurora borealis</span>'; 
+ document.getElementById('para').innerHTML = '<span> -- bought on vinyl -- <br><br>this is neil&apos;s first real EP, and the harbinger of the "modern" lemon demon era <br>its on the shorter side of lemon demon projects (~15 minutes long), but each and every song is unique and interesting :D <br> from being depressed due to snow, to actual body horror, theres a lot packed into this release <br> i also have the cd release now!! it uses a cardboard case. buyer beware! :( <br><br> 8/10 - favorite track: aurora borealis</span>'; 
+}
+
+function liveonlynot(){
+ document.getElementById('title').innerHTML= '<span>Lemon Demon - Live (Only Not)</span>';
+ document.getElementById('image').setAttribute("src", "/images/albums/liveonlynot.webp");  
+ document.getElementById('para').innerHTML = '<span> -- bought digitally -- <br><br> an interesting EP with live renditions of older lemon demon songs! songs here are mostly played live (hence the title), but with some added sfx and production. <br><br> 7/10 - favorite track: i&apos;ve got some falling to do</span>'; 
 }
 
 function viewmonster(){
  document.getElementById('title').innerHTML= '<span>Lemon Demon - View-Monster</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/jUrgOfC.jpg");  
- document.getElementById('para').innerHTML = '<span>view monster is the most developed of the "showdown era" albums, and it really shows in the atmosphere of the album. <br> to me, the texture of the entire album feels like an etheral dreamscape, with some of the songs leaning into absurdity more than others <br> i. really don&apos;t like the cardboard case that view-monster comes in. it&apos;s gorgeous, but the fact that it&apos;s cardboard means that the case will last like. what. two weeks before looking gross. <br> whatever<br><br> 10/10 - favorite track: the machine/the ocean</span>'; 
+ document.getElementById('para').innerHTML = '<span>view monster is the most developed of the "showdown era" albums, and it really shows in the atmosphere of the album. <br> to me, the texture of the entire album feels like an ethereal dreamscape, with some of the songs leaning into absurdity more than others <br> i. really don&apos;t like the cardboard case that view-monster comes in. it&apos;s gorgeous, but the fact that it&apos;s cardboard means that the case will last like. what. two weeks before looking gross. <br> whatever<br><br> 10/10 - favorite track: the machine/the ocean</span>'; 
 }
 
 function dinosaurchestra(){
  document.getElementById('title').innerHTML= '<span>Lemon Demon - Dinosaurchestra</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/tKqV7I4.jpg");  
- document.getElementById('para').innerHTML = '<span>this is the first album i <i>chose</i> to listen to in full! <br> great lyrics, amazing instrumentals, and a summery vibe make this one of my favorite albums. <br> all of that along with it being the first album i bought might have something to do with dinosaurchestra being tied my favorite album <br><br> 10/10 - favorite track: nothing worth loving <br> isnt askew</span>'; 
+ document.getElementById('para').innerHTML = '<span>this is the first album i <i>chose</i> to listen to in full! <br> great lyrics, amazing instrumentals, and a summery vibe make this one of my favorite albums. <br> all of that along with it being the first album i bought might have something to do with dinosaurchestra being tied my favorite album <br><br> 10/10 - favorite track: nothing worth loving isnt askew</span>'; 
 }
 
 function damnskippy(){
@@ -51,7 +75,7 @@ function damnskippy(){
 function httj(){
  document.getElementById('title').innerHTML= '<span>Lemon Demon - First 3 Albums (burned cds)</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/SiMVuZD.jpg");  
- document.getElementById('para').innerHTML = '<span>i burnt these 3 albums on blank cds back in 2022, so im just gonna talk about each of them here. don&apos;t wanna write more javascript! <br> hip to the javabean - most developed lemon demon album for its time! apparently, this the first studio album, and it really shows in the mixing... <br> 7/10 - favorite track: relativity <br> live from the haunted candle shop - while not amazing, i love how insane it is. only for real lemonheads<br> 8/10 - favorite track: mold el mono <br> clown circus - this was first lemon demon album, and it definitely feels like it. theres a lot of filler, but the better songs are still! pretty good! <br> 6/10 - favorite track: fire motif</span>'; 
+ document.getElementById('para').innerHTML = '<span>i burnt these 3 albums on blank cds back in 2022, so i&apos;m just gonna talk about each of them here. don&apos;t wanna write more javascript! <br> hip to the javabean - most developed lemon demon album for its time! apparently, this the first studio album, and it really shows in the mixing... <br> 7/10 - favorite track: relativity <br> live from the haunted candle shop - while not amazing, i love how insane it is. only for real lemonheads<br> 8/10 - favorite track: mold el mono <br> clown circus - this was first lemon demon album, and it definitely feels like it. there&apos;s a lot of filler, but the better songs are still! pretty good! <br> 6/10 - favorite track: fire motif</span>'; 
 }
 
 function astrolounge(){
@@ -63,13 +87,25 @@ function astrolounge(){
 function goodbye4track(){
  document.getElementById('title').innerHTML= '<span>Logan Whitewurst - Goodbye, My 4-Track</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/lZTArdP.jpg");  
- document.getElementById('para').innerHTML = '<span>this is such a silly album oh my god :3 <br> if you&apos;re having a bad day, give this album a listen!!! it has tons of soundbytes, irreverant lyrics, and a cozy texture to each song <br><br> 8/10 - favorite track: do the confusion!</span>'; 
+ document.getElementById('para').innerHTML = '<span>this is such a silly album oh my god :3 <br> if you&apos;re having a bad day, give this album a listen!!! it has tons of soundbytes, irreverent lyrics, and a cozy texture to each song <br> there&apos;s also some more sentimental songs in it, which i enjoy a LOT as well<br><br> 8/10 - favorite track: do the confusion/your brain fell out</span>'; 
 }
 
 function simpsons(){
  document.getElementById('title').innerHTML= '<span>The Simpsons Sing the Blues</span>';
  document.getElementById('image').setAttribute("src", "https://i.imgur.com/HiKEmcW.jpg");  
- document.getElementById('para').innerHTML = '<span>i dont actually like this album <br> it just feels like it was made for people to go "oh wow the simpsons music!!!! ill take 10 please". that trick worked on me as evident here <br>i just dont think the music is for me. also there&apos; the white people blues thing <br><br> 4/10 - favorite song: look at all those idiots </span>'; 
+ document.getElementById('para').innerHTML = '<span>i dont actually like this album <br> it just feels like it was made for people to go "oh wow the simpsons music!!!! ill take 10 please". that trick worked on me as evident here <br>i just don&apos;t think the music is for me. also there&apos; the white people blues thing <br><br> 4/10 - favorite song: look at all those idiots </span>'; 
+}
+
+function tommy(){
+ document.getElementById('title').innerHTML= '<span>The Who - Tommy</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/X5d6E0t.jpg");  
+ document.getElementById('para').innerHTML = '<span>while this type of music isn&apos;t exactly my thing, tommy&apos;s use of inventive storytelling and different perspectives still put it as an incredible album for me<br><br> 7/10 - favorite song: the amazing journey (specifically for the line &quot;Sickness will surely take the mind / where minds can&apos;t usually go&quot; </span>'; 
+}
+
+function tbiy(){
+ document.getElementById('title').innerHTML= '<span>Toby Fox - The Baby is You</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/48wahiA.jpeg");  
+ document.getElementById('para').innerHTML = '<span>this is just incredible i dont care go listen to it <a href=https://homestuck.net/the-baby-is-you/index.html target=_blank>now.</a><br><br> 10/10 - favorite song: A Baby Legend - The Baby is 2 </span>'; 
 }
 
 function badhairday(){
@@ -84,14 +120,21 @@ function alapalooza(){
  document.getElementById('para').innerHTML = '<span>GLUH</span>'; 
 }
 
+function selfish(){
+ document.getElementById('title').innerHTML= '<span>Will Wood - SELF-iSH</span>';
+ document.getElementById('image').setAttribute("src", "https://i.imgur.com/BGG9kVy.jpg");  
+ document.getElementById('para').innerHTML = '<span>a really great album about <i>being</i> mentally ill. from broken memories, to issues with self identity, this album is just. great! go give it a listen asap <br><br> 9/10 - favorite track: hand me my shovel, i&apos;m going in!</span>'; 
+}
+
+
 function normalalbum(){
  document.getElementById('title').innerHTML= '<span>Will Wood - The Normal Album</span>';
  document.getElementById('image').setAttribute("src", "/images/albums/normalalbum.jpg");  
- document.getElementById('para').innerHTML = '<span> -- bought digitally -- <br><br> this album is great it starts off with william jimmy tapeworm being mad and ends with him being glad his impact on the universe is negligable you <br> should go listen to it <br> also the instrumentals are varied and all great :D <br><br> 8/10 - favorite track: MARTHA THANKKYOU OFR THE MEDICINE</span>'; 
+ document.getElementById('para').innerHTML = '<span> -- bought digitally -- <br><br> this album is great it starts off with william jimmy tapeworm being mad and ends with him being glad his impact on the universe is negligible you <br> should go listen to it <br> also the instrumentals are varied and all great :D <br><br> 9/10 - favorite track: MARTHA THANKKYOU OFR THE MEDICINE</span>'; 
 }
 
 function eial(){
  document.getElementById('title').innerHTML= '<span>Will Wood - Everything is a Lot</span>';
  document.getElementById('image').setAttribute("src", "/images/albums/eial.jpg");  
- document.getElementById('para').innerHTML = '<span>--bought digitally -- <br><br> this album is... alright <br> i think i listen to will wood for the #mental illness music #not for the downward spiral of a divorced man <br> ...the instrumentals are great, if a little repetative throughout the album due to using the same ish set of instruments being used for each song <br><br> 7/10 - favorite track: front street</span>'; 
+ document.getElementById('para').innerHTML = '<span>--bought digitally -- <br><br> this album is... alright <br> i think i listen to will wood for the #mental illness music #not for the downward spiral of a divorced man <br> ...the instrumentals are great, if a little repetitive throughout the album due to using the same ish set of instruments being used for each song <br><br> 7/10 - favorite track: front street</span>'; 
 }
