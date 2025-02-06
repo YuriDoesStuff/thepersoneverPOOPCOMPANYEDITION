552d257eb81e724c2371cb4adae1dbe994e02cc9
jade floase <jadeharley0409@proton.me>
2025-02-06 05:00:01 -0600

    moved homestuck to homestuck/old... what coult this Mean?

---
+++ homestuck/old/sburb/sylladex.js
@@ -0,0 +1,18 @@
+var iframe = document.getElementById("iframe");
+
+
+function overview(){
+iframe.src = "../sburb/sylladex/overview";
+}
+
+function specubi(){
+iframe.src = "../sburb/sylladex/strifespecubi";
+}
+
+function types(){
+iframe.src = "../sburb/sylladex/types";
+}
+
+function wip(){
+iframe.src = "../wip";
+}
